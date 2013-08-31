901443ebca5c830182954b8a91d41e155c91e7f9
Pulkit Kathuria <kevincobain2000@gmail.com>
2013-09-01 01:31:21 +0900

    commiting previous logs

---
+++ main.js
@@ -0,0 +1,19 @@
+var Flickr = require("./lib/main.js");
+module.exports = Flickr;
+
+
+/*++++++++++++++++++++ Implementation Starts +++++++++++++++++++++++*/
+
+var keys = {"api_key": ""}
+flickr = new Flickr(keys);
+
+
+
+//flickr.setApi(keys);
+//flickr.getApi(function(result){
+//    console.log(result);
+//});
+
+flickr.photosSearch({"tags":"cat,dogs"}, function(result){
+    console.log(result.photos);
+});
